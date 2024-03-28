a786c425cc68eaa1281aed01f2c32707021dff61
KamranDjourshari24 <kdjour00@umd.edu>
2024-03-27 22:57:07 -0400

    Made changes so we can look at f1 stock over past month to see how the stock changes with events upcoming.

--- stockapi_explore.py
+++ stockapi_explore.py
@@ -1,12 +1,16 @@
 import requests
 import pandas as pd
 
-
+symbol = "FWONK"
+month = "2024-03"
+interval = "30"
 url = (f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol'
-       '=MSFT&interval=30min&apikey=VYT21S4Y45H54ELK')
+       f'={symbol}&interval={interval}min&month={month}&outputsize=full&'
+       f'apikey=VYT21S4Y45H54ELK')
 r = requests.get(url)
-data = r.json()["Time Series (30min)"]
+data = r.json()[f"Time Series ({interval}min)"]
 df = pd.DataFrame.from_dict(data, orient='index')
 
+print(df)
 df.to_csv("StockApi.csv", header=True,index=True)
 
