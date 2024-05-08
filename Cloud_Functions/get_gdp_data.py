b6084d19779583716eb9055e07ed020503f18523
KamranDjourshari24 <kdjour00@umd.edu>
2024-05-08 16:44:29 -0400

    Changed cloud function to now include the actual gdp value for a country in a given year along with its gdp change rate.

--- Cloud_Functions/get_gdp_data.py
+++ Cloud_Functions/get_gdp_data.py
@@ -1,28 +1,36 @@
 import requests
 import pandas as pd
-from google.cloud import storage
-from datetime import datetime
 
-def main(request):
-  gdp_lst = []
-  url = (f"https://www.imf.org/external/datamapper/api/v1/NGDP_RPCH")
-  response = requests.get(url)
-  data = response.json()["values"]["NGDP_RPCH"]
-  for country in data.items():
-    for year in country[-1]:
+chng_dict = {}
+chnge_url = (f"https://www.imf.org/external/datamapper/api/v1/NGDP_RPCH")
+response_chnge = requests.get(chnge_url)
+chng_data = response_chnge.json()["values"]["NGDP_RPCH"]
+for country in chng_data.items():
+  chng_dict[country[0]] = country[-1]
+
+
+country_codeurl = (f"https://www.imf.org/external/datamapper/api/v1/countries")
+response_country = requests.get(country_codeurl)
+countrydata = response_country.json()
+countries = list(countrydata["countries"].keys())
+
+gdp_lst = []
+
+for country in countries:
+  try:
+    gdp_url = (f"https://api.worldbank.org/v2/country/{country}/indicator/NY.GDP.MKTP.CD?format=json")
+    gdp_response = requests.get(gdp_url)
+    gdp_data = gdp_response.json()[1]
+    for item in gdp_data:
       gdp_dict = {
-          "Country_code": country[0],
-          "GDP_Change_Rate": country[-1][year],
-          "Year": year
+          "Country_code": item["countryiso3code"],
+          "Value": item['value'],
+          "Year": item['date'],
+          "GDP_Change_Rate": chng_dict[item["countryiso3code"]][item['date']]
       }
       gdp_lst.append(gdp_dict)
+  except Exception as e:
+      continue
 
-  df = pd.DataFrame(gdp_lst)
-
-
-  storage_client = storage.Client()
-  bucket = storage_client.bucket('gdp_bank')
-  filename = f'gdp_data_{datetime.now()}.csv'
-  blob = bucket.blob(filename)
-  blob.upload_from_string(df.to_csv(index=False), content_type='text/csv')
-
+df = pd.DataFrame(gdp_lst)
+print(df)
