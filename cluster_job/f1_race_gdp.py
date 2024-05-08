1c1be512d7c76f1cef0d6e4582efa17462405421
KamranDjourshari24 <kdjour00@umd.edu>
2024-05-08 16:41:15 -0400

    Adjust cluster streaming job code to add datatypes for dataframe and send the finalized cleaned dataset to Big Query as its own seperate table.

--- cluster_job/f1_race_gdp.py
+++ cluster_job/f1_race_gdp.py
@@ -1,32 +1,58 @@
-# -*- coding: utf-8 -*-
-
-!pip install pyspark
-
-import pyspark
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, functions as F
+from pyspark.sql.functions import col, to_date, substring, concat, lit
 
 # Create a SparkSession
-spark = SparkSession.builder.appName("Example").getOrCreate()
+spark = SparkSession.builder.appName("f1_race").getOrCreate()
 
 # Read in the CSV files as Spark DataFrames
-race_df = spark.read.csv("/content/F1_race_2024-05-02 18_37_43.479007.csv", header=True)
-gdp_df = spark.read.csv("/content/gdp_data_2024-04-30 20_09_34.138179.csv", header=True)
-country_df = spark.read.csv("/content/countries_code_data_2024-04-30 16_35_36.535473 (1).csv", header=True)
+race_df = spark.read.csv("gs://f1_race_info/F1_race_"
+                         "2024-05-02 18:37:43.479007.csv", header=True)
+gdp_df = spark.read.csv("gs://gdp_bank/gdp_data_2024-05-02 22:17:44.088919.csv", 
+                        header=True)
+country_df = spark.read.csv("gs://countries_code_data/countries_code_data_"
+                            "2024-04-30 16:35:36.535473.csv", header=True)
 
-#
-country_df.show()
-gdp_df.show()
-race_df.show()
-
-from pyspark.sql import functions as F
-
+# Cleaning data so the joins can be done with country name 
 race_df = race_df.withColumn("Country",
                                F.when(race_df.Country == "UK", "England")
                                .when(race_df.Country == "USA", "United States")
                                .when(race_df.Country == "Korea", "South Korea")
-                               .when(race_df.Country == "UAE", "United Arab Emirates")
+                               .when(race_df.Country == "UAE", 
+                                     "United Arab Emirates")
                                .otherwise(race_df.Country))
 
-race_df.show()
+# Joining race data with country
+race_country_df = race_df.join(country_df, race_df.Country == country_df.name, 
+                               "inner")
 
-race_country_df = race_df.join(country_df, race_df.Country == country_df.name, "inner")
+race_country_df = race_country_df.withColumn("Time", substring("Time", 1, 11))
+
+# Combine date and time (string format)
+combined_datetime = concat(col("Date"), lit(" "), col("Time"))
+
+race_country_df = race_country_df.withColumn("datetime", combined_datetime.alias("combined_datetime"))
+
+# Alias table columns with correct datatypes
+race_country_df = race_country_df.select(
+        col("Race Name").alias("Race_Name"),
+        col("datetime").cast('timestamp').alias("DateTime"),
+        col("Circuit").alias("Circuit"),
+        col("Location").alias("City"),
+        col("Country").alias("Country"),
+        col("Year").cast('integer').alias("Year"),
+        col("iso3").alias("iso3")
+)
+
+# Show the race_country_df
+race_country_df.show()
+
+# Moving DataFrame to BigQuery
+race_country_df.write.format('bigquery') \
+    .option('writeMethod', 'direct') \
+    .option('table', 'inst767-419822.finalproject.f1_race') \
+    .option('temporaryGcsBucket', 'epl-temp') \
+    .mode('overwrite') \
+    .save()
+
+# SparkSession Stopped
+spark.stop()
